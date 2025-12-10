c58253389b62a0cfc3767451f06eb8c23a95402b
eggsy-giser <eggsy2077@qq.com>
2025-12-10 09:38:11 +0800

    fix(路由): 修复登出和未登录状态的路由重定向逻辑

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -7,6 +7,7 @@ import { isCurrentUserAdmin } from "@/http/system/auth"
 import { usePermissionStore } from "@/pinia/stores/permission"
 import { useUserStore } from "@/pinia/stores/user"
 import { routerConfig } from "@/router/config"
+import { isWhiteList } from "@/router/whitelist"
 
 NProgress.configure({ showSpinner: false })
 
@@ -20,17 +21,19 @@ export function registerNavigationGuard(router: Router) {
     NProgress.start()
     const userStore = useUserStore()
     const permissionStore = usePermissionStore()
-    // 如果没有登录，直接放行并初始化路由
-    if (!getToken()) {
-      if (permissionStore.routes.length === 0) {
-        permissionStore.setAllRoutes()
-        permissionStore.addRoutes.forEach(route => router.addRoute(route))
-      }
-      if (to.path === LOGIN_PATH) return "/"
-      return true
+    const token = getToken()
+
+    // 如果没有登录
+    if (!token) {
+      // 如果在免登录的白名单中，则直接进入
+      if (isWhiteList(to)) return true
+      // 其他没有访问权限的页面将被重定向到登录页面
+      return `${LOGIN_PATH}?redirect=${encodeURIComponent(to.fullPath)}`
     }
+
     // 如果已经登录，并准备进入 Login 页面，则重定向到主页
     if (to.path === LOGIN_PATH) return "/"
+
     // 如果用户已经获得其权限角色
     if (userStore.roles.length !== 0) {
       if (permissionStore.routes.length === 0) {
